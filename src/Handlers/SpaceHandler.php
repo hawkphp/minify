97d8c8d26917a1b5e2b5a3830006a04ee0b3223c
Baimurzaev <baimurzaev@gmail.com>
2019-11-27 20:33:14 +0300

    Added space and tabulation handlers

---
+++ src/Handlers/SpaceHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hawk\Minify\Handlers;
+
+use Hawk\Minify\Interfaces\HandlerInterface;
+
+/**
+ * Class SpaceHandler
+ */
+class SpaceHandler implements HandlerInterface
+{
+    public function clear()
+    {
+        // TODO: Implement clear() method.
+    }
+}
---
+++ src/Handlers/TabulationHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hawk\Minify\Handlers;
+
+use Hawk\Minify\Interfaces\HandlerInterface;
+
+/**
+ * Class SpaceHandler
+ */
+class TabulationHandler implements HandlerInterface
+{
+    public function clear()
+    {
+
+    }
+}
