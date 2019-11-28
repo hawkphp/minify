adef072f18fd9d00950bd6b046fd9ced779498a0
Baimurzaev <baimurzaev@gmail.com>
2019-11-29 02:42:37 +0300

    Added assets for tests minify

---
+++ tests/assets/src/Handlers/TabulationHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Minify code a before deployment
+ *
+ * @author     Ruslan Baimurzaev <baimurzaev@gmail.com>
+ * @license    http://mit-license.org
+ * @link       https://github.com/hawkphp/predeploy
+ */
+namespace Hawk\Minify\Handlers;
+
+use Hawk\Minify\Interfaces\HandlerInterface;
+
+/**
+ * Class TabulationHandler
+ * @package Hawk\Minify\Handlers
+ */
+class TabulationHandler implements HandlerInterface
+{
+    public function clear()
+    {
+
+    }
+}
