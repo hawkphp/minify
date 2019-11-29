3335a163944e30d018879ef13afa01d11af549ae
Baimurzaev <baimurzaev@gmail.com>
2019-11-29 03:13:45 +0300

    Changed list assets files

---
+++ tests/assets/src/MinifyStarter.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Minify code a before deployment
+ *
+ * @author     Ruslan Baimurzaev <baimurzaev@gmail.com>
+ * @license    http://mit-license.org
+ * @link       https://github.com/hawkphp/predeploy
+ */
+
+namespace Hawk\Minify;
+
+use Hawk\Minify\Exceptions\TerminateException;
+
+/**
+ * Class MinifyStarter
+ * @package Hawk\Minify
+ */
+class MinifyStarter
+{
+    const SEPARATOR = "=========================================================";
+
+    /**
+     * @throws TerminateException
+     * @throws \Exception
+     */
+    public function squeeze()
+    {
+        $this->checkRequirements();
+
+        $runner = new Runner();
+        $runner->toBegin();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSeparator()
+    {
+        return self::SEPARATOR . PHP_EOL;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function checkRequirements()
+    {
+        if (extension_loaded("SimpleXML") === false) {
+            throw new TerminateException(
+                "The SimpleXML extension to be enabled." . PHP_EOL
+            );
+        }
+
+        if (PHP_VERSION_ID < 50400) {
+            throw new TerminateException(
+                'Minify requires PHP version 5.4 or greater.' . PHP_EOL
+            );
+        }
+    }
+}
---
+++ tests/assets/src/js/common.js
@@ -0,0 +1,6 @@
+new Slider({
+    images: '.gallery-1 .photo img',
+    next: '.gallery-1 .tabs .next',
+    prev: '.gallery-1 .tabs .prev',
+    interval: '2000'
+});
