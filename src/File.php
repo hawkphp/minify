1d0db039825848dd136908f1d814bc8caa198d3e
Baimurzaev <baimurzaev@gmail.com>
2019-12-02 03:24:21 +0300

    Added file manager

---
+++ src/File.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Hawk\Minify;
+
+use Hawk\Minify\Exceptions\TerminateException;
+
+/**
+ * Class File
+ * @package Hawk\Minify
+ */
+class File
+{
+    /**
+     * @var string|null
+     */
+    private $fileFrom;
+
+    /**
+     * @var string|null
+     */
+    private $fileTo;
+
+    /**
+     * @var string
+     */
+    private $resource;
+
+    /**
+     * File constructor.
+     * @param null $fileFrom
+     * @param null $fileTo
+     */
+    public function __construct($fileFrom = null, $fileTo = null)
+    {
+        $this->fileFrom = $fileFrom;
+        $this->fileTo = $fileTo;
+    }
+
+    /**
+     * @return $this
+     * @throws TerminateException
+     */
+    public function copy()
+    {
+        if (!copy($this->fileFrom, $this->fileTo)) {
+            throw new TerminateException('Error copying file');
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * @return $this
+     */
+    public function write()
+    {
+        if (is_file($this->fileTo) === false) {
+            throw new \InvalidArgumentException("Parameter 'pathTo' is not a file");
+        }
+
+        if (!is_dir($this->getPath())) {
+            mkdir($this->getPath(), 0775, true);
+        }
+
+        file_put_contents($this->fileTo, $this->getResource());
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function read()
+    {
+        if (is_string($this->fileFrom) === false || $this->fileFrom === '') {
+            throw new \InvalidArgumentException("File path is not a string or is empty");
+        }
+
+        if (file_exists($this->fileFrom) === false) {
+            throw new \InvalidArgumentException(sprintf("Invalid file path %s", $this->fileFrom));
+        }
+
+        $this->resource = file_get_contents($this->fileFrom);
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFileName()
+    {
+        return pathinfo($this->fileFrom, PATHINFO_FILENAME);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPath()
+    {
+        return pathinfo($this->fileFrom, PATHINFO_DIRNAME);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getExt()
+    {
+        return pathinfo($this->fileFrom, PATHINFO_EXTENSION);
+    }
+
+    /**
+     * @param $resource
+     * @return $this
+     */
+    public function setResource($resource)
+    {
+        $this->resource = $resource;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFileTo()
+    {
+        return $this->fileTo;
+    }
+
+    /**
+     * @param $fileTo
+     * @return $this
+     */
+    public function setFileTo($fileTo)
+    {
+        $this->fileTo = $fileTo;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFileFrom()
+    {
+        return $this->fileFrom;
+    }
+
+    /**
+     * @param $fileFrom
+     * @return $this
+     */
+    public function setFileFrom($fileFrom)
+    {
+        $this->fileFrom = $fileFrom;
+        return $this;
+    }
+}
